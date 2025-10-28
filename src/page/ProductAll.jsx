91b3cdd8a1868d81e16540b66fa27fe56d10d2ee
YunheeAhn <ayh95944674@gmail.com>
2025-10-28 22:52:54 +0900

    chore : my json 서버 설정

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -4,7 +4,8 @@ import ProductCard from "../component/ProductCard";
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    let url = `http://localhost:5000/products`;
+    // let url = `http://localhost:5000/products`;
+    let url = `https://my-json-server.typicode.com/YunheeAhn/shop-react`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
