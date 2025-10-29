cc42b947eff1451dc1c42f4527a5fc456e54787c
YunheeAhn <ayh95944674@gmail.com>
2025-10-29 14:17:16 +0900

    feat : 로그인 폼

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>로그인페이지</div>;
+  return (
+    <div>
+      <div className="text-cont">
+        <h3>로그인</h3>
+        <p>
+          요청하신 메뉴는 <span>로그인 후 이용 가능</span>한 메뉴 입니다.
+        </p>
+      </div>
+
+      <div className="login-cont">
+        <form action="" name="loginFrm" onSubmit={loginUser} className="login-form">
+          <div className="control">
+            <label>이메일</label>
+            <input type="text" placeholder="Enter your Email" />
+          </div>
+          <div className="control">
+            <label>비밀번호</label>
+            <input type="password" placeholder="Enter your Password" />
+          </div>
+          <div className="control">
+            <button className="login-button" type="submit">
+              로그인
+            </button>
+            <p>회원가입</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
