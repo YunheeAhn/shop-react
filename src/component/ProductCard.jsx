f4e6a6ab765f5524a170c93a3067def171097ec4
YunheeAhn <ayh95944674@gmail.com>
2025-10-30 00:26:47 +0900

    feat : Conscious choice 참 거짓 판단하여 노출

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -9,10 +9,11 @@ const ProductCard = ({ item }) => {
   return (
     <dl className="card-box" onClick={showDetail}>
       <dt>
-        <img src={item?.img} alt="" />
+        <img src={item?.img} alt={item?.title} />
         <p className="new">{item?.new == true ? "신제품" : ""}</p>
       </dt>
       <dd>
+        <span>{item?.choice ? "Conscious choice" : ""}</span>
         <h3>{item?.title}</h3>
         <p>{item?.price}원</p>
       </dd>
