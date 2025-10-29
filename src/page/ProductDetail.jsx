fb23e692a16de321c4c09eb7f18c077109ac4658
YunheeAhn <ayh95944674@gmail.com>
2025-10-29 16:17:51 +0900

    feat : 디테일 페이지 해당 id값에 따른 my json 데이터 받아오기

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const ProductDetail = () => {
-  return <div>상품상세페이지</div>;
+  let { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  const getProductDetail = async () => {
+    let url = `https://my-json-server.typicode.com/YunheeAhn/shop-react/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    setProduct(data);
+  };
+
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
+  return (
+    <section className="section pro-view">
+      <div className="pro-cont">
+        <dl>
+          <dt>
+            <img src={product?.img} alt={product?.title} />
+          </dt>
+          <dd>
+            <span>{product?.title}</span>
+            <h3>{product?.menu}</h3>
+            <p>{product?.price}원</p>
+            <div className="choice">
+              {product?.size?.map((size, index) => (
+                <button key={index}>{size}</button>
+              ))}
+            </div>
+          </dd>
+        </dl>
+      </div>
+      <div className="detail-info">
+        <h4>제품 상세정보</h4>
+        <img src={product?.detailImg} alt={product?.title} />
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetail;
