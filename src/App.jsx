64f667baef84d020c90973445807bb3816984d2a
YunheeAhn <ayh95944674@gmail.com>
2025-10-29 14:18:01 +0900

    feat : 로그인 상태 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router";
-import ProductAll from "./page/ProductAll";
-import LoginPage from "./page/LoginPage";
-import ProductDetail from "./page/ProductDetail";
 import "./App.css";
 import Navbar from "./component/Navbar";
+import ProductAll from "./page/ProductAll";
+import ProductDetail from "./page/ProductDetail";
+import LoginPage from "./page/LoginPage";
 
 // 1. 전체 상품페이지, 로그인페이지, 상품 상세페이지
 // 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다
@@ -13,6 +14,9 @@ import Navbar from "./component/Navbar";
 // 6. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
 // 7. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없고, 다시 로그인 페이지를 보여준다.
 function App() {
+  // 로그인 한 유저인지 아닌지 상태 확인
+  const [authenticate, setAuthenticate] = useState(false); // false->로그인X
+
   return (
     <>
       <header>
