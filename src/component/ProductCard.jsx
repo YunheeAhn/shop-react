b5f0d5d4e82048080553f446f720e55a075c7183
YunheeAhn <ayh95944674@gmail.com>
2025-10-29 15:01:45 +0900

    feat : 상품 클릭시 상품 디테일 페이지 이동

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,6 +1,13 @@
+import React from "react";
+import { useNavigate } from "react-router";
+
 const ProductCard = ({ item }) => {
+  const navigate = useNavigate();
+  const showDetail = () => {
+    navigate(`/product/${item.id}`);
+  };
   return (
-    <dl className="card-box">
+    <dl className="card-box" onClick={showDetail}>
       <dt>
         <img src={item?.img} alt="" />
         <p className="new">{item?.new == true ? "신제품" : ""}</p>
